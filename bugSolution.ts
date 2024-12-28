1912d654ca8bef3b75bbfa8e2215e6934fa6df2e
BugHunter-X <askzenai@gmail.com>
2024-12-28 06:38:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,9 @@
+function greeter(person: string): string {
+  return "Hello, " + person;
+}
+
+let user = ["Jane User", "John User"];
+
+user.forEach(person => {
+  console.log(greeter(person));
+}); //Correct
